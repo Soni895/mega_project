37851997a372dae7cb48783ca9d2a30b467cef21
soni895 <darshansoni895@gmail.com>
2023-08-08 20:46:39 +0530

    SubSection completed

---
+++ models/SubSection.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+const SubSectionSchema=   new mongoose.Schema(
+    {
+        Title:
+        {
+            type:String,
+        },
+        TimeDuration:
+
+        {
+            type:String,
+             
+        },
+        Description:
+        {
+            type:String,
+        },
+        VideoUrl:
+        {
+            type:String,
+        }
+
+        
+        
+
+    }
+);
+const SubSection=mongoose.model("SubSection",SubSectionSchema);
+module.exports=SubSection;
