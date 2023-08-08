8ef12216a261f2bd484c72c6dd83a84f9e5d39cc
soni895 <darshansoni895@gmail.com>
2023-08-08 21:03:43 +0530

    Course Schema completed

---
+++ models/course.js
@@ -0,0 +1,79 @@
+const mongoose=require("mongoose");
+const CourseSchema=new mongoose.Schema(
+    {
+        CourseName:
+        {
+            type:String,
+            required:true,
+            trim:true,
+        },
+        CourseDescription:
+        {
+            type:String,
+            required:true,
+            trim:true,
+        },
+        Instructor:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+         WhatYouWillLearn:
+         {
+            type:String,
+            required:true
+         },
+
+         CourseContent:
+         [
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Section",
+                required:true
+
+            }
+         ],
+         RatingAndREview:
+        [
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"RatingAndREview",
+                required:true
+
+            }
+        ],
+        Price:
+        {
+            type:Number,
+            required:true,
+        },
+        Thumbnail:
+
+        {
+            type:String,
+            required:true,
+        },
+        Tag:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Tag",
+            required:true
+
+        },
+        StudentEnrolled:
+        [
+            {
+
+                
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+            }
+            
+        ]
+
+    }
+);
+const Course=mongoose.model("Course",CourseSchema);
+module.exports=Course;
