89be3f8b8a95ac0ce126870d42af1965bc90ea69
soni895 <darshansoni895@gmail.com>
2023-08-08 19:09:40 +0530

    user model completed

---
+++ models/user.js
@@ -0,0 +1,65 @@
+const mongoose=require("mongoose");
+const UserSchema=new mongoose.Schema(
+ 
+    {
+        FirstName:
+        {
+            type:String,
+            required:true,
+            trim:true,
+
+        },
+        LastName:
+        {
+            type:String,
+            required:true,
+            trim:true,
+
+        },
+        Email:
+        {
+            type:String,
+            required:true,
+         
+
+        },
+        AccountType:
+        {
+            type:String,
+            required:true,
+           enum: ["Admin","Student","Instructor"],
+        },
+
+        AdditionalDetails:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"Profile",
+        },
+        Courses:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                required:true,
+                ref:"Profile",
+            }
+        ],
+        Image:
+        {
+            type:String,
+            required:true,
+        },
+        CourseProgrss:
+        [
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                required:true,
+                ref:"CourseProgress",
+                
+            }
+        ]
+
+    }
+);
+const user= mongoose.model("User",UserSchema);
+
+module.exports=user;
