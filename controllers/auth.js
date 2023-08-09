4d1bfd0a93cf77ff3f00a7b2c524a22d2a129e61
soni895 <darshansoni895@gmail.com>
2023-08-09 20:10:31 +0530

    Signup Completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,12 +6,12 @@ const User=require("../models/user");
 const Otp=require("../models/otp");
 const otpGenerator = require('otp-generator');
 const uniqid = require('uniqid');
+const bcrypt=require("bcrypt");
+const Profile=require("../models/profile");
 
 exports.SendOtp=async(req, res)=>
 {
     try {
-
-    
     // fetch emial
     const {Email}=req.body;
 
@@ -67,4 +67,164 @@ exports.SendOtp=async(req, res)=>
     )
     
 }
+}
+
+// sign up fucntion
+
+exports.SignUp= async (req,res)=>
+{
+    try {
+
+        // data fetch
+        // validdate 
+        // 2nd password matchMediacheck user already exiss
+        // find most recet otp 
+        // password hashed
+        // save in db
+        const {
+            AccountType,
+            Email,FirstName,
+            LastName,
+            Image,
+            Password,
+            ContactNumber,
+            Otp,
+            ConfirmPassword
+        }=req.body;
+console.log( AccountType,
+    Email,FirstName,
+    LastName,
+    Image,
+    Password,
+    ContactNumber,
+    Otp,
+    ConfirmPassword);
+        if( 
+            !Email|| 
+            !FirstName||
+            !LastName||
+            !Image||
+            !Password||
+            !ContactNumber||
+            !Otp||
+            !ConfirmPassword)
+            {
+               return res.status(403).json(
+                {
+                    success:false,
+                    status:"Unsuccessful",
+                    message:"All field are required to fill"
+                }
+               ) 
+            }
+
+            if(Password!==ConfirmPassword)
+            {
+                return res.status(400).json(
+                    {
+                        success:false,
+                        status:"Unsuccessful",
+                        message:"Password and confirmPassword not match"
+                    }
+                )
+
+            }
+            const isUserPresent= await User.findOne({Email});
+            if(isUserPresent)
+            {
+                return res.status(400).json(
+                    {
+                        success:false,
+                        status:"Unsuccessful",
+                        message:"existing user please login"
+                    }
+                )
+            }
+
+            const recentotp= await  Otp.find({
+                Email,
+            }).sort({
+                CreatedAt:-1
+
+            }).limit(1);
+            console.log("recentotp =>",recentotp); 
+            if(recentotp.length===0)
+            {
+                return{
+                    success:false,
+                    status:"Unsuccessful",
+                    message:"Otp not found"
+                }
+            }else if(Otp!==recentotp)
+            {
+                return res.status(400).json(
+                    {
+                        success:false,
+                        status:"Unsuccessful",
+                        message:"Otp not match"
+                    }
+                )
+            }
+            let hashedpassword;
+            try{
+                hashedpassword= await bcrypt.hash(Password,10);
+    
+            }
+            catch(error)
+            {
+                return res.status(400).json(
+                    {
+                        status:"error in hasing password",
+                        success:false,
+    
+                    }
+                )
+            }
+            const profile= await Profile.create({
+                ContactNumber:null,
+                About:null,
+                DateOfBirth:null,
+                Gender:null
+            })
+            const Payload= new User(
+                {
+                    AccountType,
+                    Email,FirstName,
+                    LastName,
+                    Image:`https://api.dicebear.com/5.x/initials/svg?seed=${FirstName} ${LastName}`,
+                    Password:hashedpassword,
+                    ContactNumber,
+                    AdditionalDetails:profile._id
+                  
+                }
+            )
+
+         
+            const response= await Payload.save();
+            return res.status(200).json(
+                {
+                    status:true,
+                    message:"user Signup successful",
+                    response,
+                    Payload,
+                    hashedpassword,
+ 
+                }
+            )
+
+
+
+
+        
+    } catch (error) {
+        return res.status(500).json(
+            {
+                status:false,
+                message:"try again",
+              error,
+            }
+        )
+        
+    }
+    
 }
