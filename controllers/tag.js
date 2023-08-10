7e213d68f1834725c729ba21f488c05972013ef7
soni895 <darshansoni895@gmail.com>
2023-08-10 19:03:23 +0530

    tag completed

--- controllers/tag.js
+++ controllers/tag.js
@@ -20,6 +20,7 @@ exports.CreateTag=async (req,res)=>
                 Name,Description
             }
           );
+          console.log(Tag_Deatils);
           return res.status(200).json(
             {
                 status:true,
@@ -38,4 +39,32 @@ exports.CreateTag=async (req,res)=>
           )
         
     }
+}
+
+exports.GetAllTag= async (req,res)=>
+{
+    try {
+        const response= await Tag.find({},{name:true,Description:true});
+        console.log(response);
+        return res.status(200).json({
+            Success:true,
+            Status:"successful",
+            Message:"find All tag successful",
+            response,
+
+
+        })
+
+        
+    } catch (error) {
+        return res.status(500).json(
+            {
+                status:false,
+                error,
+            }
+          )
+         
+    }
+
+
 }
