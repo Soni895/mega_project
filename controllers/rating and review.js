348325a68ca029f7a1bf68a33b57eec4abaf121d
soni895 <darshansoni895@gmail.com>
2023-08-14 22:06:53 +0530

    Createrating ahndler completed

--- controllers/rating and review.js
+++ controllers/rating and review.js
@@ -1,6 +1,91 @@
 // create ratig
 // get rating
 // get all rating
+  const RatingAndREview= require("../models/ratingandreview");
+  const Course= require("../models/course");
+
+  exports.Createrating= async (req,res)=>
+  {
+    try {
+        // get user id
+        // fetch  data from req body
+        // check is user is enroll or not
+        // check already review and rating
+        // create RatingAndREview
+        // update course with this
+        // return  response
+        const UserId= req.User.id;
+        const {rating,Review,CourseId}= req.body;
+        const CourseDetailes= await Course.findOne({_id:CourseId,
+        StudentEnrolled:{$elemMatch:{$eq:UserId}}
+                                                  });
+
+    if(!CourseDetailes)
+    {
+       
+                return res.status(404).json(
+                    {
+                        status:"Unsuccessful",
+                        success:false,
+                        
+                        message:"Course and user not found",
+                    }
+                )   
+    }
+
+    const ISAlreadyReviewed= RatingAndREview.findOne(
+        {
+            User:UserId,
+            Course:CourseId,
+        }
+    )
+     
+    if(ISAlreadyReviewed)
+    {
+        return res.status(404).json(
+            {
+                status:"Unsuccessful",
+                success:false,
+           
+                message:"Course is alraedy review by user ",
+            }
+        )  
+    }
+
+    const response= await  RatingAndREview.create(
+        {
+            Rating,
+            Review,
+            USer:UserId,
+
+        }
+    );
+    const Updated_Course=  await Course.findByIdAndUpdate({
+        _id:UserId
+    },
+    {
+    $push:{RatingAndREview:response._id}
+    },{new :true});
+    
+    return res.status(200).json(
+        {
+            Success:true,
+            message:"rating and review successful Created",
+            response,
+            Updated_Course
 
 
-exports.Createrating
+        }
+    )
+
+    } 
+    catch (error) {
+        return res.status(500).json(
+            {
+                Staus:false,
+                error,
+                message:" error in craeting rating and review",
+            }
+        )
+        }
+    }
