65d2e75f741d282aa916cbe85c8724077df271d5
soni895 <darshansoni895@gmail.com>
2023-11-19 13:40:36 +0530

    code modifed

--- models/user.js
+++ models/user.js
@@ -88,6 +88,6 @@ const UserSchema=new mongoose.Schema(
 
     }
 );
-const user= mongoose.model("User",UserSchema);
+const User= mongoose.model("User",UserSchema);
 
-module.exports=user;
+module.exports=User;
