c952372c672a18a8fc10d733f92cbb0ff7abd1ad
soni895 <darshansoni895@gmail.com>
2023-12-04 11:15:40 +0530

    instructor handler completed

--- controllers/profile.js
+++ controllers/profile.js
@@ -357,6 +357,6 @@ exports.InstructorDashboard= async (req,res)=>
       } catch (error) {
         console.error(error)
         res.status(500).json({ message: "Server Error" })
-      }
+    }
 
 }
